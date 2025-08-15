3ea01a4f7a7d49277368bf514e35496bf44a6767
HeAmaral10 <hefacebook100@gmail.com>
2025-08-15 14:53:33 -0300

    Uso de localStorage no contador

--- revisao/src/pages/Contador.jsx
+++ revisao/src/pages/Contador.jsx
@@ -1,16 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import "./Contador.css";
 
 function Contador() {
-    const [contador, setContador] = useState(0);
-    
+
+    const [contador, setContador] = useState(() => {
+        const valorSalvo = localStorage.getItem("contador");
+        return valorSalvo !== null ? Number(valorSalvo) : 0;
+    });
+
+    useEffect(() => {
+        localStorage.setItem("contador", contador);
+    }, [contador]);
+
     return (
         <div>
-        <h1>Contador</h1>
-        <p>Valor atual: {contador}</p>
-        <button onClick={() => setContador(contador + 1)}>Somar</button>
-        <button onClick={() => setContador(contador - 1)}>Subtrair</button>
-        <button onClick={() => setContador(0)}>Zerar</button>
+            <h1>Contador</h1>
+            <p>Valor atual: {contador}</p>
+            <button onClick={() => setContador(contador + 1)}>Somar</button>
+            <button onClick={() => {
+                if (contador > 0) {
+                    setContador(contador - 1);
+                }}}> Subtrair </button>
+            <button onClick={() => setContador(0)}>Zerar</button>
         </div>
     );
 }
